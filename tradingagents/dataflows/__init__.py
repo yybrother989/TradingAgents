1a784b34eb72b9964ed83c717c7415939470e853
Leo (Yue) Gao <yue.gao1@outlook.com>
2025-09-29 11:54:56 -0400

    Fix __init__.py compatibility with new interface.py
    - Update imports to use new routing system
    - Remove references to deprecated functions
    - Ensure compatibility with Alpha Vantage modules

--- tradingagents/dataflows/__init__.py
+++ tradingagents/dataflows/__init__.py
@@ -5,42 +5,18 @@ from .reddit_utils import fetch_top_from_category
 from .stockstats_utils import StockstatsUtils
 from .yfin_utils import YFinanceUtils
 
-from .interface import (
-    # News and sentiment functions
-    get_finnhub_news,
-    get_finnhub_company_insider_sentiment,
-    get_finnhub_company_insider_transactions,
-    get_google_news,
-    get_reddit_global_news,
-    get_reddit_company_news,
-    # Financial statements functions
-    get_simfin_balance_sheet,
-    get_simfin_cashflow,
-    get_simfin_income_statements,
-    # Technical analysis functions
-    get_stock_stats_indicators_window,
-    get_stockstats_indicator,
-    # Market data functions
-    get_YFin_data_window,
-    get_YFin_data,
-)
+# Import routing functions from interface
+from .interface import route_to_vendor, get_vendor, get_category_for_method
 
 __all__ = [
-    # News and sentiment functions
-    "get_finnhub_news",
-    "get_finnhub_company_insider_sentiment",
-    "get_finnhub_company_insider_transactions",
-    "get_google_news",
-    "get_reddit_global_news",
-    "get_reddit_company_news",
-    # Financial statements functions
-    "get_simfin_balance_sheet",
-    "get_simfin_cashflow",
-    "get_simfin_income_statements",
-    # Technical analysis functions
-    "get_stock_stats_indicators_window",
-    "get_stockstats_indicator",
-    # Market data functions
-    "get_YFin_data_window",
-    "get_YFin_data",
+    # Core utilities
+    "YFinanceUtils",
+    "StockstatsUtils", 
+    "get_data_in_range",
+    "getNewsData",
+    "fetch_top_from_category",
+    # Interface routing
+    "route_to_vendor",
+    "get_vendor",
+    "get_category_for_method",
 ]
